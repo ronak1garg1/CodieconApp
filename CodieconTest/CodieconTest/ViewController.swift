34ed2d3f34060256534f6bfb4b76a490d8d9160e
Ronak Garg <41041523+ronak1garg1@users.noreply.github.com>
2019-03-08 17:50:32 +0530

    Add files via upload
    first commit

---
+++ CodieconTest/CodieconTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodieconTest
+//
+//  Created by RONAK GARG on 08/03/19.
+//  Copyright © 2019 Codiecon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
